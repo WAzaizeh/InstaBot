126721005586031041cfd0b2f763f15d281544b7
WAzaizeh <wesam.azaizeh@gmail.com>
2020-04-27 17:38:54 -0400

    Changed csvHandler initialization

--- csvHandler.py
+++ csvHandler.py
@@ -10,15 +10,15 @@ class csvHandler(object):
     def __init__(self):
         if Constants.DATABASE_PATH == '': # if no path is specified, save in the same file
             Constants.DATABASE_PATH = os.getcwd() + '/' + Constants.INST_USER + '.csv'
-            try:
-                if not os.path.isfile(Constants.DATABASE_PATH):
-                    headers = ['username', 'date']
-                    with open(Constants.DATABASE_PATH, 'a+') as f:
-                        writer = csv.writer(f)
-                        writer.writerow(headers)
-                    Constants.update_settings_file() # save into the settings file
-            except OSError: # to handle error when file exists and cannot be accessed
-                pass
+        try:
+            if not os.path.isfile(Constants.DATABASE_PATH):
+                headers = ['username', 'date']
+                with open(Constants.DATABASE_PATH, 'a+') as f:
+                    writer = csv.writer(f)
+                    writer.writerow(headers)
+                Constants.update_settings_file() # save into the settings file
+        except OSError: # to handle error when file exists and cannot be accessed
+            pass
 
     # add new user
     def add_user(username):
